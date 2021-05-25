91e945cc1498bf5bf8c6afa62c63976fb50a6d47
Sai Bhargav <saibhargav97v@gmail.com>
2021-05-25 20:10:17 +0530

    Added decorator pattern example in Java

---
+++ decorator/ShapeDecorator.java
@@ -0,0 +1,11 @@
+package designpatterns.decorator;
+
+public abstract class ShapeDecorator implements Shape{
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape) {
+        this.decoratedShape = decoratedShape;
+    }
+
+    abstract public void draw();
+}
